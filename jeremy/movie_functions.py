fcd4f87d50214af104416b36659ec29cddafadd7
bartojer <bartojer@gmail.com>
2025-02-03 12:33:58 -0700

    fix custom_df() and supporting functions

--- jeremy/movie_functions.py
+++ jeremy/movie_functions.py
@@ -3,17 +3,10 @@ import pandas as pd
 dataset = "../movies_cleaned.csv"
 df = pd.read_csv(dataset)
 
-columns = df.columns
-for c in columns:
-    print(c)
+# columns = df.columns
+# for c in columns:
+#     print(c)
 
-
-def list_columns():
-    i = 0
-    for column in columns:
-        print(i + 1, column)
-        i += 1
-    return i
 
 def select_column():
     count = list_columns()
@@ -32,28 +25,37 @@ def select_columns():
         selection = input("> ")
     return chosen_list
 
-def select_columns_limit(amount):
-    column_count = list_columns()
+
+
+
+def list_columns(df):
+    i = 0
+    columns = df.columns
+    for column in columns:
+        print(i + 1, column)
+        i += 1
+    return columns
+
+def select_columns_limit(amount, df):
+    columns = list_columns(df)
     count = 0
-    print(f"choose column (1 - {column_count})")
-    print("type a number and press enter. type 0 when finished.")
+    print(f"choose column (1 - {len(columns)})")
+    print("type a number and press enter.")
     chosen_list = []
-    while count <= amount:
+    while count < amount:
+        selection = input("> ")
         chosen_list.append(int(selection) - 1)
         count += 1
-
         if count == amount:
             continue
-        selection = input("> ")
     
     return chosen_list
 
 
 
+def custom_df(df, columns=2):
+    chosen_columns = select_columns_limit(columns, df)
+    new_df = df.iloc[:10, chosen_columns]
+    return new_df.to_string(index=False)
 
-
-
-def custom_df(df):
-    chosen_columns = select_columns_limit(2)
-    new_df = df[chosen_columns]
-    return new_df
+print(custom_df(df))
