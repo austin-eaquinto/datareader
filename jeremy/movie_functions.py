296afe79a2662ea443b3b69a2fd719386f6a1545
bartojer <bartojer@gmail.com>
2025-02-10 07:34:37 -0700

    update movie_functions

--- jeremy/movie_functions.py
+++ jeremy/movie_functions.py
@@ -45,27 +45,34 @@ def list_columns(df):
         i += 1
     return columns
 
-def select_columns_limit(amount, df):
+def select_columns_limit(df, column_limit):
     columns = list_columns(df)
     count = 0
     print(f"choose column (1 - {len(columns)})")
     print("type a number and press enter.")
     chosen_list = []
-    while count < amount:
+    while count < column_limit:
         selection = input("> ")
         chosen_list.append(int(selection) - 1)
         count += 1
-        if count == amount:
+        if count == column_limit:
             continue
     
     return chosen_list
 
 
 
+<<<<<<< Updated upstream
 def custom_df(df, columns=2):
     chosen_columns = select_columns_limit(columns, df)
     new_df = df.iloc[:, chosen_columns]
     return new_df
 # print(custom_df(df))
+=======
+def custom_df(df, column_limit=2):
+    chosen_columns = select_columns_limit(df, column_limit)
+    new_df = df.iloc[:10, chosen_columns]
+    return new_df.to_string(index=False)
+>>>>>>> Stashed changes
 
 # custom_df(df).to_csv("id_name", index=False)
